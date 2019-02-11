fcaf17c1a8cec634e134faf684ecf6392475c369
seank76 <seank76@gmail.com>
2019-02-10 20:30:46 -0500

    initial commit

---
+++ src/main/scala/com/upsorok/JsonSupport.scala
@@ -0,0 +1,19 @@
+package com.upsorok
+
+import com.upsorok.user.UserRegistryActor.ActionPerformed
+import com.upsorok.user.{User, Users}
+
+//#json-support
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.DefaultJsonProtocol
+
+trait JsonSupport extends SprayJsonSupport {
+  // import the default encoders for primitive types (Int, String, Lists etc)
+  import DefaultJsonProtocol._
+
+  implicit val userJsonFormat = jsonFormat3(User)
+  implicit val usersJsonFormat = jsonFormat1(Users)
+
+  implicit val actionPerformedJsonFormat = jsonFormat1(ActionPerformed)
+}
+//#json-support
